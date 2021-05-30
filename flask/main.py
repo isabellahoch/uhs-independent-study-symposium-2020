96cbcf7687f456bbcb27a5049bb8d9d7309a237a
isabella hochschild <ihochschild@gmail.com>
2021-05-30 14:52:07 -0700

    2021 updates

--- flask/main.py
+++ flask/main.py
@@ -145,106 +145,108 @@ def get_info():
     all_info.pop(0)
     all_info.pop(0)
     for item in all_info:			
-        this_item = {"timestamp":item[0], "email":item[1], "name":item[2], "graduation_year":item[3], "sponsor":item[4], "department":item[5], "title":item[6], "description":item[7], "link":item[8], "cover_photo":item[9], "profile_photo":item[10], "zoom_link":item[11], "proj_type":item[12], "sharing_confirmation":item[13], "if_synchronous":item[14], "additional_comments":item[15], "random":item[16], "group_names":item[17], "random_2":item[18], "link_2":item[19], "if_slideshow":item[20], "id":item[21], "new_link":item[22], "david":item[23], "session_1":item[24], "session_2":item[25], "session_3":item[26], "sponsor_email":item[27]}
+        this_item = {"timestamp":item[0], "email":item[1], "name":item[2],"graduation_year":item[3], "group_names":item[4],"sponsor":item[5], "department":item[6], "title":item[7], "description":item[8], "link":item[10], "cover_photo":item[13], "profile_photo":item[14], "zoom_link":item[16], "proj_type":item[12], "sharing_confirmation":item[13], "if_synchronous":item[15], "additional_comments":item[15], "random":item[16], "group_names":"", "random_2":item[18], "link_2":item[19], "if_slideshow":item[20], "id":item[21], "new_link":item[22], "david":item[23], "session_1":item[24], "session_2":item[25], "session_3":item[26], "sponsor_email":item[27]}
         this_item["index"] = all_info.index(item)
-        if "," in this_item["name"]:
-            this_item["names"] = this_item["name"].split(", ")
-        this_item["name_info"] = []
-        info["is_names"].append(this_item["title"].lower())
-        # for leader in this_item["leader_array"]:
-        #     leader_dict = {"name": leader,"link":"https://www.sfuhs.org/student/community-directory?utf8=%E2%9C%93&const_search_role_ids=6%2C2%2C1&const_search_first_name="+leader.split(" ")[0]+"&const_search_last_name="+leader.split(" ")[1]}
-        #     if "and " in leader:
-        #         leader_dict["name"] = leader[4:]
-        #     if this_item["leader_array"].index(leader) == len(this_item["leader_array"])-1 and len(this_item["leader_array"])!=1:
-        #         leader_dict["is_not_last"] = False
-        #     else:
-        #         leader_dict["is_not_last"] = True
-        #     if this_item["leader_array"].index(leader) != 0:
-        #         leader_dict["is_not_first"] = True
-        #     this_item["leader_info"].append(leader_dict)
-        this_item["sponsor_array"] = [this_item["sponsor"]]
-        this_item["sponsor_info"] = []
-        for sponsor in this_item["sponsor_array"]:
-            if " " in sponsor:
-                sponsor_dict = {"name": sponsor,"link":"https://www.sfuhs.org/student/community-directory?utf8=%E2%9C%93&const_search_role_ids=6%2C2%2C1&const_search_first_name="+sponsor.split(" ")[0]+"&const_search_last_name="+sponsor.split(" ")[1]}
-            else:
-                sponsor_dict = {"name": sponsor,"link":"https://www.sfuhs.org/student/community-directory?utf8=%E2%9C%93&const_search_role_ids=6%2C2%2C1&const_search_first_name="+sponsor}
-            if "and " in sponsor:
-                sponsor_dict["name"] = sponsor[4:]
-            if this_item["sponsor_array"].index(sponsor) == len(this_item["sponsor_array"])-1:
-                sponsor_dict["is_not_last"] = False
-            else:
-                sponsor_dict["is_not_last"] = True
-            if this_item["sponsor_array"].index(sponsor) != 0:
-                sponsor_dict["is_not_first"] = True
-            if len(this_item["sponsor_array"])==1:
-                sponsor_dict["is_not_last"] = False
-                sponsor_dict["is_not_first"] = False
-            this_item["sponsor_info"].append(sponsor_dict)
-        this_item["short_description"] = this_item["description"][:250]
-        for x in range(250, 0, -1):
-            if len(this_item["short_description"]) > 2:
-                try:
-                    new_variable = this_item["short_description"][x-1]
-                    if  new_variable == " ":
-                        break
-                    else:
-                        this_item["short_description"] = this_item["short_description"][:x-1]
-                except:
-                    print(this_item["title"]+" description error")
+        print(this_item['name'])
+        if this_item["title"]:
+            if "," in this_item["name"]:
+                this_item["names"] = this_item["name"].split(", ")
+            this_item["name_info"] = []
+            info["is_names"].append(this_item["title"].lower())
+            # for leader in this_item["leader_array"]:
+            #     leader_dict = {"name": leader,"link":"https://www.sfuhs.org/student/community-directory?utf8=%E2%9C%93&const_search_role_ids=6%2C2%2C1&const_search_first_name="+leader.split(" ")[0]+"&const_search_last_name="+leader.split(" ")[1]}
+            #     if "and " in leader:
+            #         leader_dict["name"] = leader[4:]
+            #     if this_item["leader_array"].index(leader) == len(this_item["leader_array"])-1 and len(this_item["leader_array"])!=1:
+            #         leader_dict["is_not_last"] = False
+            #     else:
+            #         leader_dict["is_not_last"] = True
+            #     if this_item["leader_array"].index(leader) != 0:
+            #         leader_dict["is_not_first"] = True
+            #     this_item["leader_info"].append(leader_dict)
+            this_item["sponsor_array"] = [this_item["sponsor"]]
+            this_item["sponsor_info"] = []
+            for sponsor in this_item["sponsor_array"]:
+                if " " in sponsor:
+                    sponsor_dict = {"name": sponsor,"link":"https://www.sfuhs.org/student/community-directory?utf8=%E2%9C%93&const_search_role_ids=6%2C2%2C1&const_search_first_name="+sponsor.split(" ")[0]+"&const_search_last_name="+sponsor.split(" ")[1]}
+                else:
+                    sponsor_dict = {"name": sponsor,"link":"https://www.sfuhs.org/student/community-directory?utf8=%E2%9C%93&const_search_role_ids=6%2C2%2C1&const_search_first_name="+sponsor}
+                if "and " in sponsor:
+                    sponsor_dict["name"] = sponsor[4:]
+                if this_item["sponsor_array"].index(sponsor) == len(this_item["sponsor_array"])-1:
+                    sponsor_dict["is_not_last"] = False
+                else:
+                    sponsor_dict["is_not_last"] = True
+                if this_item["sponsor_array"].index(sponsor) != 0:
+                    sponsor_dict["is_not_first"] = True
+                if len(this_item["sponsor_array"])==1:
+                    sponsor_dict["is_not_last"] = False
+                    sponsor_dict["is_not_first"] = False
+                this_item["sponsor_info"].append(sponsor_dict)
+            this_item["short_description"] = this_item["description"][:250]
+            for x in range(250, 0, -1):
+                if len(this_item["short_description"]) > 2:
+                    try:
+                        new_variable = this_item["short_description"][x-1]
+                        if  new_variable == " ":
+                            break
+                        else:
+                            this_item["short_description"] = this_item["short_description"][:x-1]
+                    except:
+                        print(this_item["title"]+" description error")
 
-               
-        this_item["short_description"] = this_item["short_description"].strip()+"..."
-        # if len(item)>=11:
-        #     if item[10] == "Yes":
-        #         this_item["is_affinity_group"] = True
-        if "?id=" in this_item["cover_photo"]:
-            this_item["cover_photo"] = "https://drive.google.com/uc?export=view&id="+this_item["cover_photo"].split("?id=")[1]
-        if "?id=" in this_item["profile_photo"]:
-            this_item["profile_photo"] = "https://drive.google.com/uc?export=view&id="+this_item["profile_photo"].split("?id=")[1]
-        # if "presentation" in this_item["link"]:
-        #     this_item["embed_link"] = '<iframe src="https://drive.google.com/embeddedfolderview?id='+this_item["link"].split("/folders/")[1].split("?")[0]+'/#grid" frameborder="0" width="1440" height="839" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>'
-        if this_item["new_link"] != "":
-            this_item["link"] = this_item["new_link"]
-        if this_item["group_names"] != "":
-            this_item["name"] = this_item["group_names"]
-        else:
-            this_item["group_names"] = None
-        if "folder" in this_item["link"]:
-            this_item["link"] = "https://drive.google.com/embeddedfolderview?id="+this_item["link"].split("/folders/")[1].split("?")[0]+"#grid"
-        if "/pub" in this_item["link"]:
-            if "presentation" in this_item["link"]:
-                this_item["link"] = this_item["link"].split("/pub")[0]+"/embed?start=true&loop=true&delayms=10000"
-            elif "document" in this_item["link"]:
-                this_item["link"] = this_item["link"] + "?embedded=true"
-        if this_item["link_2"] == "":
-            this_item["link_2"] = None
-        if this_item["david"] == "":
-            this_item["david"] = None
-        if "/view" in this_item["link"]:
-            this_item["link"] = this_item["link"].replace("view","preview")
-        if this_item["cover_photo"] == "":
-            this_item["cover_photo"] = "https://via.placeholder.com/150?text="+this_item["title"]+" ("+this_item["name"]+")"
-        info["is_array"].append(this_item)
-        info["is_dict"][this_item["id"]] = this_item
-        this_item["department_id"] = this_item["department"].lower().replace(" ","-")
-        if this_item["department_id"] == "human-development":
-            this_item["department_id"] = "hd"
-        if this_item["department_id"] not in info["category_is_dict"]:
-            info["category_is_dict"][this_item["department_id"]] = []
-        if this_item["zoom_link"] != "":
-            info["zoom_array"].append(this_item)
-        info["category_is_dict"][this_item["department_id"]].append(this_item)
-        random.shuffle(info["category_is_dict"][this_item["department_id"]]) 
-        if "user" in info:
-            if info["user"]["email"] == this_item["email"]:
-                if "independent_studies" not in info["user"]:
-                    info["user"]["independent_studies"] = []
-                info["user"]["independent_studies"].append(this_item)
-            elif info["user"]["email"] == this_item["sponsor_email"] or info["user"]["email"] in this_item["sponsor_email"].split(", "):
-                if "sponsor_studies" not in info["user"]:
-                    info["user"]["sponsor_studies"] = []
-                info["user"]["sponsor_studies"].append(this_item)
+                
+            this_item["short_description"] = this_item["short_description"].strip()+"..."
+            # if len(item)>=11:
+            #     if item[10] == "Yes":
+            #         this_item["is_affinity_group"] = True
+            if "?id=" in this_item["cover_photo"]:
+                this_item["cover_photo"] = "https://drive.google.com/uc?export=view&id="+this_item["cover_photo"].split("?id=")[1]
+            if "?id=" in this_item["profile_photo"]:
+                this_item["profile_photo"] = "https://drive.google.com/uc?export=view&id="+this_item["profile_photo"].split("?id=")[1]
+            # if "presentation" in this_item["link"]:
+            #     this_item["embed_link"] = '<iframe src="https://drive.google.com/embeddedfolderview?id='+this_item["link"].split("/folders/")[1].split("?")[0]+'/#grid" frameborder="0" width="1440" height="839" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>'
+            if this_item["new_link"] != "":
+                this_item["link"] = this_item["new_link"]
+            if this_item["group_names"] != "":
+                this_item["name"] = this_item["group_names"]
+            else:
+                this_item["group_names"] = None
+            if "folder" in this_item["link"]:
+                this_item["link"] = "https://drive.google.com/embeddedfolderview?id="+this_item["link"].split("/folders/")[1].split("?")[0]+"#grid"
+            if "/pub" in this_item["link"]:
+                if "presentation" in this_item["link"]:
+                    this_item["link"] = this_item["link"].split("/pub")[0]+"/embed?start=true&loop=true&delayms=10000"
+                elif "document" in this_item["link"]:
+                    this_item["link"] = this_item["link"] + "?embedded=true"
+            if this_item["link_2"] == "":
+                this_item["link_2"] = None
+            if this_item["david"] == "":
+                this_item["david"] = None
+            if "/view" in this_item["link"]:
+                this_item["link"] = this_item["link"].replace("view","preview")
+            if this_item["cover_photo"] == "":
+                this_item["cover_photo"] = "https://via.placeholder.com/150?text="+this_item["title"]+" ("+this_item["name"]+")"
+            info["is_array"].append(this_item)
+            info["is_dict"][this_item["id"]] = this_item
+            this_item["department_id"] = this_item["department"].lower().replace(" ","-")
+            if this_item["department_id"] == "human-development":
+                this_item["department_id"] = "hd"
+            if this_item["department_id"] not in info["category_is_dict"]:
+                info["category_is_dict"][this_item["department_id"]] = []
+            if this_item["zoom_link"] != "":
+                info["zoom_array"].append(this_item)
+            info["category_is_dict"][this_item["department_id"]].append(this_item)
+            random.shuffle(info["category_is_dict"][this_item["department_id"]]) 
+            if "user" in info:
+                if info["user"]["email"] == this_item["email"]:
+                    if "independent_studies" not in info["user"]:
+                        info["user"]["independent_studies"] = []
+                    info["user"]["independent_studies"].append(this_item)
+                elif info["user"]["email"] == this_item["sponsor_email"] or info["user"]["email"] in this_item["sponsor_email"].split(", "):
+                    if "sponsor_studies" not in info["user"]:
+                        info["user"]["sponsor_studies"] = []
+                    info["user"]["sponsor_studies"].append(this_item)
     random.shuffle(info["is_array"])   
     random.shuffle(info["zoom_array"])    
     return info 
